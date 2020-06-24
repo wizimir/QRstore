ad5bbdf897e2978f79eaf6b6c8b8a3038f4cf259
wizimir <lukasz.j.krawiec@gmail.com>
2020-06-24 15:28:53 +0200

    working camera and qr detection

--- app/src/main/java/com/example/qrstore/MainActivity.java
+++ app/src/main/java/com/example/qrstore/MainActivity.java
@@ -1,14 +1,107 @@
 package com.example.qrstore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView svBarcode;
+    private TextView tvBarcode;
+    private BarcodeDetector detector;
+    private CameraSource cameraSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        svBarcode = findViewById(R.id.svBarcode);
+        tvBarcode = findViewById(R.id.tvBarcode);
+        detector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
+        detector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barCodeTable = detections.getDetectedItems();
+                if (barCodeTable.size() > 0) {
+                    tvBarcode.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvBarcode.setText(barCodeTable.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
+        cameraSource = new CameraSource.Builder(this, detector).setAutoFocusEnabled(true).setRequestedPreviewSize(1024, 768).setRequestedFps(25f).build();
+        svBarcode.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        cameraSource.start(holder);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 123);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                try {
+                    cameraSource.start(svBarcode.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        detector.release();
+        cameraSource.stop();
+        cameraSource.release();
     }
 }
+
+
